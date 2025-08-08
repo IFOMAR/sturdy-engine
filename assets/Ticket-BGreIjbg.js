35cfed60b982e4b5e3649995d346e13f24773a26
IFOMAR <112605989+IFOMAR@users.noreply.github.com>
2025-08-08 11:08:00 +0300

    Create Ticket-BGreIjbg.js

---
+++ assets/Ticket-BGreIjbg.js
@@ -0,0 +1,229 @@
+import {S as w, i as A, s as z, Q as C, R as Z, T as c, m as u, U as M, c as V, b as S, V as T, W as B, X as U, Y as W, t as q, d as D, h as _, Z as k, _ as P, $ as Q, D as R, p as h} from "./index-BsTNpDop.js";
+function X(r) {
+    let e = (console.error('Unsupported icon weight. Choose from "thin", "light", "regular", "bold", "fill", or "duotone".'),
+    ""), t;
+    return {
+        c() {
+            t = R(e)
+        },
+        m(l, i) {
+            V(l, t, i)
+        },
+        p: h,
+        d(l) {
+            l && _(t)
+        }
+    }
+}
+function Y(r) {
+    let e;
+    return {
+        c() {
+            e = c("path"),
+            u(e, "d", "M232,100a4,4,0,0,0,4-4V64a12,12,0,0,0-12-12H32A12,12,0,0,0,20,64V96a4,4,0,0,0,4,4,28,28,0,0,1,0,56,4,4,0,0,0-4,4v32a12,12,0,0,0,12,12H224a12,12,0,0,0,12-12V160a4,4,0,0,0-4-4,28,28,0,0,1,0-56ZM28,192V163.78a36,36,0,0,0,0-71.56V64a4,4,0,0,1,4-4H92V196H32A4,4,0,0,1,28,192Zm168-64a36.06,36.06,0,0,0,32,35.78V192a4,4,0,0,1-4,4H100V60H224a4,4,0,0,1,4,4V92.22A36.06,36.06,0,0,0,196,128Z")
+        },
+        m(t, l) {
+            V(t, e, l)
+        },
+        p: h,
+        d(t) {
+            t && _(e)
+        }
+    }
+}
+function j(r) {
+    let e;
+    return {
+        c() {
+            e = c("path"),
+            u(e, "d", "M232,104a8,8,0,0,0,8-8V64a16,16,0,0,0-16-16H32A16,16,0,0,0,16,64V96a8,8,0,0,0,8,8,24,24,0,0,1,0,48,8,8,0,0,0-8,8v32a16,16,0,0,0,16,16H224a16,16,0,0,0,16-16V160a8,8,0,0,0-8-8,24,24,0,0,1,0-48ZM32,167.2a40,40,0,0,0,0-78.4V64H88V192H32Zm192,0V192H104V64H224V88.8a40,40,0,0,0,0,78.4Z")
+        },
+        m(t, l) {
+            V(t, e, l)
+        },
+        p: h,
+        d(t) {
+            t && _(e)
+        }
+    }
+}
+function E(r) {
+    let e;
+    return {
+        c() {
+            e = c("path"),
+            u(e, "d", "M232,102a6,6,0,0,0,6-6V64a14,14,0,0,0-14-14H32A14,14,0,0,0,18,64V96a6,6,0,0,0,6,6,26,26,0,0,1,0,52,6,6,0,0,0-6,6v32a14,14,0,0,0,14,14H224a14,14,0,0,0,14-14V160a6,6,0,0,0-6-6,26,26,0,0,1,0-52ZM30,192V165.53a38,38,0,0,0,0-75.06V64a2,2,0,0,1,2-2H90V194H32A2,2,0,0,1,30,192Zm196-26.47V192a2,2,0,0,1-2,2H102V62H224a2,2,0,0,1,2,2V90.47a38,38,0,0,0,0,75.06Z")
+        },
+        m(t, l) {
+            V(t, e, l)
+        },
+        p: h,
+        d(t) {
+            t && _(e)
+        }
+    }
+}
+function F(r) {
+    let e;
+    return {
+        c() {
+            e = c("path"),
+            u(e, "d", "M232,104a8,8,0,0,0,8-8V64a16,16,0,0,0-16-16H32A16,16,0,0,0,16,64V96a8,8,0,0,0,8,8,24,24,0,0,1,0,48,8,8,0,0,0-8,8v32a16,16,0,0,0,16,16H224a16,16,0,0,0,16-16V160a8,8,0,0,0-8-8,24,24,0,0,1,0-48ZM32,167.2a40,40,0,0,0,0-78.4V64H88V192H32Z")
+        },
+        m(t, l) {
+            V(t, e, l)
+        },
+        p: h,
+        d(t) {
+            t && _(e)
+        }
+    }
+}
+function G(r) {
+    let e, t;
+    return {
+        c() {
+            e = c("path"),
+            t = c("path"),
+            u(e, "d", "M200,128a32,32,0,0,0,32,32v32a8,8,0,0,1-8,8H96V56H224a8,8,0,0,1,8,8V96A32,32,0,0,0,200,128Z"),
+            u(e, "opacity", "0.2"),
+            u(t, "d", "M232,104a8,8,0,0,0,8-8V64a16,16,0,0,0-16-16H32A16,16,0,0,0,16,64V96a8,8,0,0,0,8,8,24,24,0,0,1,0,48,8,8,0,0,0-8,8v32a16,16,0,0,0,16,16H224a16,16,0,0,0,16-16V160a8,8,0,0,0-8-8,24,24,0,0,1,0-48ZM32,167.2a40,40,0,0,0,0-78.4V64H88V192H32Zm192,0V192H104V64H224V88.8a40,40,0,0,0,0,78.4Z")
+        },
+        m(l, i) {
+            V(l, e, i),
+            V(l, t, i)
+        },
+        p: h,
+        d(l) {
+            l && (_(e),
+            _(t))
+        }
+    }
+}
+function I(r) {
+    let e;
+    return {
+        c() {
+            e = c("path"),
+            u(e, "d", "M232,108a12,12,0,0,0,12-12V64a20,20,0,0,0-20-20H32A20,20,0,0,0,12,64V96a12,12,0,0,0,12,12,20,20,0,0,1,0,40,12,12,0,0,0-12,12v32a20,20,0,0,0,20,20H224a20,20,0,0,0,20-20V160a12,12,0,0,0-12-12,20,20,0,0,1,0-40ZM36,170.34a44,44,0,0,0,0-84.68V68H88V188H36Zm184,0V188H112V68H220V85.66a44,44,0,0,0,0,84.68Z")
+        },
+        m(t, l) {
+            V(t, e, l)
+        },
+        p: h,
+        d(t) {
+            t && _(e)
+        }
+    }
+}
+function J(r) {
+    let e, t, l, i;
+    const H = r[7].default
+      , o = C(H, r, r[6], null);
+    function p(a, n) {
+        return a[0] === "bold" ? I : a[0] === "duotone" ? G : a[0] === "fill" ? F : a[0] === "light" ? E : a[0] === "regular" ? j : a[0] === "thin" ? Y : X
+    }
+    let m = p(r)
+      , s = m(r)
+      , g = [{
+        xmlns: "http://www.w3.org/2000/svg"
+    }, {
+        width: r[2]
+    }, {
+        height: r[2]
+    }, {
+        fill: r[1]
+    }, {
+        transform: l = r[3] ? "scale(-1, 1)" : void 0
+    }, {
+        viewBox: "0 0 256 256"
+    }, r[4], r[5]]
+      , d = {};
+    for (let a = 0; a < g.length; a += 1)
+        d = Z(d, g[a]);
+    return {
+        c() {
+            e = c("svg"),
+            o && o.c(),
+            t = c("rect"),
+            s.c(),
+            u(t, "width", "256"),
+            u(t, "height", "256"),
+            u(t, "fill", "none"),
+            M(e, d)
+        },
+        m(a, n) {
+            V(a, e, n),
+            o && o.m(e, null),
+            S(e, t),
+            s.m(e, null),
+            i = !0
+        },
+        p(a, [n]) {
+            o && o.p && (!i || n & 64) && T(o, H, a, a[6], i ? U(H, a[6], n, null) : B(a[6]), null),
+            m === (m = p(a)) && s ? s.p(a, n) : (s.d(1),
+            s = m(a),
+            s && (s.c(),
+            s.m(e, null))),
+            M(e, d = W(g, [{
+                xmlns: "http://www.w3.org/2000/svg"
+            }, (!i || n & 4) && {
+                width: a[2]
+            }, (!i || n & 4) && {
+                height: a[2]
+            }, (!i || n & 2) && {
+                fill: a[1]
+            }, (!i || n & 8 && l !== (l = a[3] ? "scale(-1, 1)" : void 0)) && {
+                transform: l
+            }, {
+                viewBox: "0 0 256 256"
+            }, a[4], n & 32 && a[5]]))
+        },
+        i(a) {
+            i || (q(o, a),
+            i = !0)
+        },
+        o(a) {
+            D(o, a),
+            i = !1
+        },
+        d(a) {
+            a && _(e),
+            o && o.d(a),
+            s.d()
+        }
+    }
+}
+function K(r, e, t) {
+    const l = ["weight", "color", "size", "mirrored"];
+    let i = k(e, l)
+      , {$$slots: H={}, $$scope: o} = e;
+    const {weight: p, color: m, size: s, mirrored: g, ...d} = P("iconCtx") || {};
+    let {weight: a=p ?? "regular"} = e
+      , {color: n=m ?? "currentColor"} = e
+      , {size: v=s ?? "1em"} = e
+      , {mirrored: b=g || !1} = e;
+    return r.$$set = f => {
+        e = Z(Z({}, e), Q(f)),
+        t(5, i = k(e, l)),
+        "weight"in f && t(0, a = f.weight),
+        "color"in f && t(1, n = f.color),
+        "size"in f && t(2, v = f.size),
+        "mirrored"in f && t(3, b = f.mirrored),
+        "$$scope"in f && t(6, o = f.$$scope)
+    }
+    ,
+    [a, n, v, b, d, i, o, H]
+}
+class N extends w {
+    constructor(e) {
+        super(),
+        A(this, e, K, J, z, {
+            weight: 0,
+            color: 1,
+            size: 2,
+            mirrored: 3
+        })
+    }
+}
+export {N as T};
